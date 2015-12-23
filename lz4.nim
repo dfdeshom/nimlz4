0dc58c2c7666e9a3f29ec9bf9473f7917f9a5b7f
dfdeshom <didier@cogtree.com>
2015-12-22 21:32:01 -0600

    clean up frame uncompress operation after it's done

--- lz4.nim
+++ lz4.nim
@@ -85,6 +85,10 @@ proc uncompress*(source:string):string =
    
   result = dest
 
+#
+# LZ4 Frame API
+#
+
 proc newLZ4F_frameInfo*():LZ4F_frameInfo =
   var info:LZ4F_frameInfo
   info.blockSizeID = LZ4F_blockSizeID.LZ4F_default
@@ -93,6 +97,23 @@ proc newLZ4F_frameInfo*():LZ4F_frameInfo =
   info.frameType = LZ4F_frameType.LZ4F_frame
   info.contentSize = 0
   result = info
+
+proc newLZ4F_preferences*(frame_info:LZ4F_frameInfo,
+                         compressionLevel:int=0,
+                         autoFlush:int=1):LZ4F_preferences =
+  var res:LZ4F_preferences
+  res.frameInfo = frame_info
+  res.compressionLevel = cint(compressionLevel)
+  res.autoFlush = cuint(autoFlush)
+  result = res
+
+proc newLZ4F_preferences*(compressionLevel:int=0,
+                         autoFlush:int=1):LZ4F_preferences =
+  var res:LZ4F_preferences
+  res.frameInfo = newLZ4F_frameInfo()
+  res.compressionLevel = cint(compressionLevel)
+  res.autoFlush = cuint(autoFlush)
+  result = res
 
 proc blockSizeId_to_bytes(b:LZ4F_blockSizeID): int =
   ## Get the right number of bytes from a `LZ4F_blockSizeID`
@@ -108,9 +129,6 @@ proc blockSizeId_to_bytes(b:LZ4F_blockSizeID): int =
   of LZ4F_max4MB:
     result = 4 * 1024 * 1024
 
-#
-# LZ4 Frame API
-#
 
 # Simple frame compression and decompression
 proc compress_frame*(source: var string,
@@ -146,115 +164,108 @@ proc compress_frame*(source: var string,
     
 proc uncompress_frame*(source: var string): string =
   ## decompress a string that uses the LZ4 frame format
-
-  # TODO: wrap in try/catch statement so that
-  # resources get freed cleanly
-  
-  # create decompression context
+      
   var dcontext:LZ4F_decompressionContext
-  let context_status = LZ4F_createDecompressionContext(addr(dcontext),
-                                                       cuint(LZ4F_VERSION))
-  if LZ4F_isError(context_status) == 1:
-    let error = LZ4F_getErrorName(context_status)
-    raise newException(LZ4Exception,$error)
-
-  # make source into a char ptr
-  var src_size:int = source.len
-  var csource = cast[ptr char](alloc0(sizeof(char) * src_size))
-  copyMem(csource,addr(source[0]),Natural(src_size))
+  var dest:ptr char = nil
+  var csource: ptr char = nil
   
-  var options:LZ4F_decompressOptions
-  var start = 0
-  var stop = src_size
-    
-  # try to get frame header info to allocate
-  # the right size for the destination buffer
-  var frame = newLZ4F_frameInfo()
-  let initial_hint = LZ4F_getFrameInfo(dcontext,
-                                      addr(frame),
-                                      csource,
-                                      addr(src_size))
-  if LZ4F_isError(initial_hint) == 1:
-      let error = LZ4F_getErrorName(initial_hint)
+  try:
+    let context_status = LZ4F_createDecompressionContext(addr(dcontext),
+                                                         cuint(LZ4F_VERSION))
+    if LZ4F_isError(context_status) == 1:
+      let error = LZ4F_getErrorName(context_status)
       raise newException(LZ4Exception,$error)
-
-  # if frame.contentSize is set, use that
-  # else get max frame size from the blockSizeID
-  var dest_size:int
-  var block_size:int
-  let content_size = int(frame.contentSize)
-  if  content_size > 0:
-    dest_size = content_size
-  else:
-    block_size = blockSizeId_to_bytes(frame.blockSizeID)
-    dest_size = block_size
-    
-  var dest:ptr char = cast[ptr char](alloc0(sizeof(char) * dest_size))
   
-  # after calling `LZ4F_getFrameInfo`, we must move
-  # to the next bytes
-  start = src_size
+    # make source into a char ptr
+    var src_size:int = source.len
+    csource = cast[ptr char](alloc0(sizeof(char) * src_size))
+    copyMem(csource,addr(source[0]),Natural(src_size))
 
-  result = newString(dest_size)
-  # index of the last element of the result buffer
-  var resbeg = 0
-  # size so far of the result buffer
-  var cumulative_size = dest_size
-  while true:
-    let hint_src_size_bytes = LZ4F_decompress(dcontext,
-                                              dest,
-                                              addr(dest_size),
-                                              csource.offset(start),
-                                              addr(stop),
-                                              addr(options))
+    var start = 0
+    var stop = src_size
 
-    if LZ4F_isError(hint_src_size_bytes ) == 1:
-      let error = LZ4F_getErrorName(hint_src_size_bytes)
-      raise newException(LZ4Exception,$error)
+    # try to get frame header info to allocate
+    # the right size for the destination buffer
+    var frame = newLZ4F_frameInfo()
+    let initial_hint = LZ4F_getFrameInfo(dcontext,
+                                        addr(frame),
+                                        csource,
+                                        addr(src_size))
+    if LZ4F_isError(initial_hint) == 1:
+        let error = LZ4F_getErrorName(initial_hint)
+        raise newException(LZ4Exception,$error)
 
-    if hint_src_size_bytes == 0:
-      break
-  
-    start += stop
-    stop = hint_src_size_bytes
-    
+    # if frame.contentSize is set, use that
+    # else get max frame size from the blockSizeID
+    var dest_size:int
+    var block_size:int
+    let content_size = int(frame.contentSize)
+    if  content_size > 0:
+      dest_size = content_size
+    else:
+      block_size = blockSizeId_to_bytes(frame.blockSizeID)
+      dest_size = block_size
+
+    dest = cast[ptr char](alloc0(sizeof(char) * dest_size))
+
+    # after calling `LZ4F_getFrameInfo`, we must move
+    # to the next bytes
+    start = src_size
+
+    result = newString(dest_size)
+    # index of the last element of the result buffer
+    var resbeg = 0
+    # size so far of the result buffer
+    var cumulative_size = dest_size
+    # LZ4 options
+    var options:LZ4F_decompressOptions
+    while true:
+      let hint_src_size_bytes = LZ4F_decompress(dcontext,
+                                                dest,
+                                                addr(dest_size),
+                                                csource.offset(start),
+                                                addr(stop),
+                                                addr(options))
+
+      if LZ4F_isError(hint_src_size_bytes ) == 1:
+        let error = LZ4F_getErrorName(hint_src_size_bytes)
+        raise newException(LZ4Exception,$error)
+
+      if hint_src_size_bytes == 0:
+        break
+
+      start += stop
+      stop = hint_src_size_bytes
+
+      moveMem(addr(result[resbeg]),dest,dest_size)
+      resbeg += dest_size 
+      cumulative_size += dest_size
+      result.setLen(resbeg+dest_size)
+      zeroMem(dest,dest_size)
+
+    # # we are done, free the context
+    # let free_status = LZ4F_freeDecompressionContext(dcontext)
+    # if LZ4F_isError(free_status) == 1:
+    #   let error = LZ4F_getErrorName(free_status)
+    #   raise newException(LZ4Exception,$error)
+
     moveMem(addr(result[resbeg]),dest,dest_size)
-    resbeg += dest_size 
-    cumulative_size += dest_size
-    result.setLen(resbeg+dest_size)
-    zeroMem(dest,dest_size)
+
+    # when done reading the source buffer, the dest buffer
+    # might only be partially-filled. Get exactly
+    # how many spaces to chop up from the result buffer
+    let unfilled_space = block_size - dest_size
+    result.setLen(cumulative_size-unfilled_space)
+
+  finally:
     
-  # we are done, free the context
-  let free_status = LZ4F_freeDecompressionContext(dcontext)
-  if LZ4F_isError(free_status) == 1:
-    let error = LZ4F_getErrorName(free_status)
-    raise newException(LZ4Exception,$error)
+    if dest != nil:
+      dealloc(dest)
+      
+    if csource != nil:  
+      dealloc(csource)
 
-  moveMem(addr(result[resbeg]),dest,dest_size)
-  
-  # when done reading the source buffer, the dest buffer
-  # might only be partially-filled. Get exactly
-  # how many spaces to chop up from the result buffer
-  let unfilled_space = block_size - dest_size
-  result.setLen(cumulative_size-unfilled_space)
-  
-  dealloc(dest)
-  dealloc(csource)
-    
-proc newLZ4F_preferences*(frame_info:LZ4F_frameInfo,
-                         compressionLevel:int=0,
-                         autoFlush:int=1):LZ4F_preferences =
-  var res:LZ4F_preferences
-  res.frameInfo = frame_info
-  res.compressionLevel = cint(compressionLevel)
-  res.autoFlush = cuint(autoFlush)
-  result = res
-
-proc newLZ4F_preferences*(compressionLevel:int=0,
-                         autoFlush:int=1):LZ4F_preferences =
-  var res:LZ4F_preferences
-  res.frameInfo = newLZ4F_frameInfo()
-  res.compressionLevel = cint(compressionLevel)
-  res.autoFlush = cuint(autoFlush)
-  result = res
-
+    let free_status = LZ4F_freeDecompressionContext(dcontext)
+    if LZ4F_isError(free_status) == 1:
+      let error = LZ4F_getErrorName(free_status)
+      raise newException(LZ4Exception,$error)
